5e1a61ba93c4e6c1d5492db22fbbd82ecdc1b8eb
TheoMinier <>
2019-04-04 18:39:55 +0200

    Remise en place des route album

--- projet/myApp/routes/album.js
+++ projet/myApp/routes/album.js
@@ -8,7 +8,19 @@ const Album = require("../controllers/album.controller.js");
 // Create RAW data array
 
 
+/* GET albums listing. */
+router.get('/', Album.findAll);
 
+/* GET one album. */
+router.get('/:id',Album.findOne);
+
+
+/* PUT new album. */
+router.put('/',Album.create);
+
+
+/* DELETE artiste. */
+router.delete('/:id',Album.delete);
 
 /* UPDATE album. */
 router.post('/:id', Album.update);
